8ed62459947499071373245f7ca416766df7cc94
roze512 <rizu9513@gmail.com>
2026-07-17 01:48:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Portfolio website loaded
+console.log("Rizwan Portfolio Website Loaded Successfully!");
+
+// Welcome message
+window.addEventListener("load", function () {
+    console.log("Welcome to Rizwan Safdar's Portfolio");
+});
