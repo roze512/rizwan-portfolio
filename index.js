3c9f990a920adb021ce46c7a5701e251a66863cc
roz512 <rizu9513@gmail>
2026-07-22 15:58:48 +0500

    Add mobile navigation menu

--- index.js
+++ index.js
@@ -33,3 +33,9 @@ window.addEventListener("scroll", () => {
     }
 
 });
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click",()=>{
+    navLinks.classList.toggle("active");
+});
